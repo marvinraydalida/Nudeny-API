6c75efcecd66ac1d7e016b77f492e3495e7fcb92
marvinraydalida <marvinraydalida@gmail.com>
2022-11-26 03:23:18 +0800

    changed classification endpoint to classify

--- main.py
+++ main.py
@@ -11,20 +11,8 @@ app = FastAPI()
 classification_model = NudenyClassify()
 detection_model = NudenyDetect()
 
-@app.get("/")
-async def main():
-    content = """
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
 
-
-@app.post("/uploadfiles/")
+@app.post("/classify/")
 async def create_upload_files(files: list[UploadFile]):
     return {"predictions": [classification_model.classify(await file.read(), file.filename) for file in files]}
 
